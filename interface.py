8f24ba08278bbdff3a9ac127258c8d969b14960e
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-10-30 09:26:40 +0000

    masalah transfer sukses

--- interface.py
+++ interface.py
@@ -1,4 +1,8 @@
 import cmd,json,requests
+from peewee import *
+from models import *
+
+database = SqliteDatabase('bank.db')
 
 class Interface(cmd.Cmd) :
 	
@@ -61,16 +65,95 @@ class Interface(cmd.Cmd) :
 				print ("ping success")
 		except (Exception,KeyError) as e :
 			print ('error :', e)
+	def do_transfer(self,line):
+		arg = line.split(" ")
+		api = "http://" + arg[0] + "/ewallet/transfer"
+		user_id = arg[1]
+		nominal = int(arg[2])
+		input = {"user_id":user_id,"nilai":nominal}
+		input = json.dumps(input)
+		try :
+			response = json.loads(requests.post(api, data = input).text)
+			print (response)
+		except Exception as e:
+			print (e)
+		try :
+			if response['status_transfer'] == 1 :
+				transfer_success(user_id,nominal)
+				print ('Transfer sukses')
+			elif response['status_transfer'] == -2 :
+				print ('quorum tidak terpenuhi')
+			elif response['status_transfer'] == -1 :
+				print ('user_id belum terdaftar')
+			elif response['status_transfer'] == -4 :
+				print ('kesalahan database')
+			elif response['status_transfer'] == -5 :
+				print ('nilai transfer diluar ketentuan')
+			elif response['status_transfer'] == -99 :
+				print ('kesalahan belum terdefinisi')
+		except (Exception,KeyError) as e:
+			print ('error :',e)
+
+	def transfer_success(user_id,nominal):
+		database.connect()
+		try :
+			nasabah = Nasabah.get(npm=user_id)
+			nasabah.saldo -= nominal
+			print ("saldo anda : ",nasabah.saldo)
+			nasabah.save()
+		except Exception as e:
+			print (e)
+		database.close()
+
+	def do_simpan(self,line):
+		arg = line.split(" ")
+		user_id = arg[0]
+		nominal = int(arg[1])
+		database.connect()
+		try :
+			nasabah = Nasabah.get(npm=user_id)
+			nasabah.saldo += nominal
+			nasabah.save()
+			print ("berhasil menambahkan saldo, saldo anda : ", nasabah.saldo)
+		except Exception as e:
+			print (e)
+
+	def do_totalSaldo(self,line):
+		arg = line.split(" ")
+		api = "http://" + arg[0] + "/ewallet/getTotalSaldo"
+		user_id = arg[1]
+		input = {"user_id":user_id}
+		input = json.dumps(input)
+		try :
+			response = json.loads(requests.post(api, data = input).text)
+		except Exception as e:
+			print (e)
+		try :
+			if response['nilai_saldo'] == -2 :
+				print ('quorum tidak terpenuhi')
+			elif response['nilai_saldo'] == -1 :
+				print ('user_id belum terdaftar')
+			elif response['nilai_saldo'] == -3 :
+				print ('pemanggilan host tidak berhasil')
+			elif response['nilai_saldo'] == -99 :
+				print ('kesalahan belum terdefinisi')
+			else :
+				print ('saldo anda ',response['nilai_saldo'])
+		except (Exception,KeyError) as e:
+			print ('error :',e)
 
 	def do_EOF(self, line):
 		return True
 
 if __name__ == '__main__':
-	print ('<<<<<<<<<<<<<<<<<<<<E-WALLET>>>>>>>>>>>>>>>>>>>>')
-	print ('================================================')
+	print ('<<<<<<<<<<<<<<<<<<<<<<<<<E-WALLET>>>>>>>>>>>>>>>>>>>>>>>>>')
+	print ('==========================================================')
 	print ('PETUNJUK :')
 	print ('untuk registrasi = register <IP> <NAMA> <USER_ID>')
 	print ('untuk cek saldo = saldo <IP> <USER_ID>')
 	print ('untuk ping = ping <IP>')
-	print ('================================================')
+	print ('untuk transfer = transfer <IP> <USER_ID> <NOMINAL>')
+	print ('untuk menambahkan saldo = simpan <USER_ID> <NOMINAL>')
+	print ('untuk mendapatkan total saldo = totalSaldo <IP> <USER_ID>')
+	print ('==========================================================')
 	Interface().cmdloop()
--- tugas1.py
+++ tugas1.py
@@ -2,10 +2,11 @@ from flask import Flask,request,jsonify
 from peewee import *
 from models import *
 import requests
-import json,sys
+import json,sys,socket
 
 app = Flask(__name__)
 database = SqliteDatabase('bank.db')
+hostname = socket.gethostbyname(socket.gethostname())
 
 def get_quorum():
 	try :
@@ -36,7 +37,7 @@ def get_quorum():
 			except (Exception,KeyError) as e :
 				print ('ping not success in ',ip,' error :',e)
 	vote = (n/(len(quorum))) * 100
-	print ('hasil quorum :',vote)
+	print ('Hasil quorum :',vote)
 	return vote
 
 @app.before_request
@@ -101,18 +102,19 @@ def transfer():
 	if vote < 50 :
 		return jsonify(status_transfer=-2),200
 	else :
-		 try :
+		try :
                         user_id = json.loads(request.data.decode("utf-8"))['user_id']
-                except Exception as e:
+		except Exception as e:
                         print ("error : ",sys.exc_info())
                         return jsonify(status_transfer=-99),200
-                try :
+		try :
                         nasabah = Nasabah.get(npm=user_id)
-                except Exception as e:
+		except Exception as e:
                         print (e)
                         return jsonify(status_transfer=-1),200
 		try :
 			nominal = json.loads(request.data.decode("utf-8"))["nilai"]
+			nominal = int(nominal)
 		except Exception as e:
 			print (e)
 			return jsonify(status_transfer=-99),200
@@ -125,6 +127,68 @@ def transfer():
 		except Exception as e:
 			print (e)
 			return jsonify(status_transfer=-4),200
+
+def get_cabang():
+	try :
+                #response = json.loads(requests.get('http://152.118.31.2/list.php').text)
+                response = json.loads(requests.get('http://www.mocky.io/v2/59df65fa0f00001009173c3f').text)
+	except Exception as e:
+                print (e)
+	cabang = {}
+	for user in response :
+		ip=user['ip']
+		user_id=user['npm']
+		cabang[user_id]=ip
+	return cabang
+
+def cek_cabang(id):
+        cabang = get_cabang()
+        if cabang[id] is hostname:
+                return 1
+        else :
+                return 0
+
+@app.route('/ewallet/getTotalSaldo',methods=['POST'])
+def getTotalSaldo():
+	vote = get_quorum()
+	if vote is not 100:
+		return jsonify(nilai_saldo=-2),200
+	else:	
+		try :
+                        user_id = json.loads(request.data.decode("utf-8"))['user_id']
+		except Exception as e:
+                        print ("error : ",sys.exc_info())
+                        return jsonify(nilai_saldo=-99),200
+		try :
+                        nasabah = Nasabah.get(npm=user_id)
+		except Exception as e:
+                        print (e)
+                        return jsonify(status_transfer=-1),200
+		cabang = get_cabang()
+		if cek_cabang(user_id) is 0 :
+			request_total = 'http://' + cabang[user_id] + '/ewallet/getTotalSaldo'
+			try :
+				post_data={"user_id":user_id}
+				post_data=json.dumps(post_data)
+				total_saldo = json.loads(requests.post(request_total, data = post_data).text)
+			except Exception as e:
+				print (e)
+				return jsonify(nilai_saldo=-99),200
+			return jsonify(nilai_saldo=total_saldo['nilai_saldo']),200
+		else :
+			sum = 0.0
+			for bank in cabang :
+				api = 'http://' + bank['ip'] + '/ewallet/getSaldo'
+				post_data = {"user_id":user_id}
+				post_data = json.dumps(post_data)
+				try :
+					saldo = json.loads(requests.post(api,data = post_data).text)
+				except Exception as e: 
+					print (e)
+					return jsonify(nilai_saldo=-3),200
+				if saldo['nilai_saldo'] > 0 :
+					sum += saldo['nilai_saldo']
+			return jsonify(nilai_saldo=sum),200
 
 if __name__ == '__main__':
     app.run(threaded=True,port=80,host='0.0.0.0')
